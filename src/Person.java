96e1be8742a5f8dcf26394e313a677a4ee5983ca
Dmytro Kulish <mitiakylish2@gmail.com>
2024-12-24 23:12:35 +0200

    Added abstract class and methods

--- src/Person.java
+++ src/Person.java
@@ -1,37 +1,26 @@
-public class Person implements Displayable{
-    private String Name;
-    private int age;
-
-    private PersonRole job;
-
+public abstract class Person implements Displayable {
+    protected String Name;
+    protected int age;
+    protected PersonRole job;
 
     public Person(String Name, int age, PersonRole job) {
         this.Name = Name;
         this.age = age;
         this.job = job;
     }
-    public String getName() {
-        return Name;
-    }
-    public int getAge() {
-        return age;
-    }
-    public PersonRole getJob() {
-        return job;
-    }
 
-    public void setName(String Name){
-        this.Name = Name;
-    }
-    public void setAge(int age){
-        this.age = age;
-    }
-    public void setJob(PersonRole job){
-        this.job = job;
-    }
+    public abstract String getName();
+
+    public abstract int getAge();
+
+    public abstract PersonRole getJob();
+
+    public abstract void setName(String Name);
+
+    public abstract void setAge(int age);
+
+    public abstract void setJob(PersonRole job);
 
     @Override
-    public void displayInformation() {
-        System.out.println("Ім'я: " + Name + ", Вік: " + age + ", Професія: " + job);
-    }
+    public abstract void displayInformation();
 }
--- src/Woman.java
+++ src/Woman.java
@@ -5,6 +5,36 @@ public class Woman extends Person{
     }
 
     @Override
+    public String getName() {
+        return Name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public PersonRole getJob() {
+        return job;
+    }
+
+    @Override
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public void setJob(PersonRole job) {
+        this.job = job;
+    }
+
+    @Override
     public void displayInformation(){
         System.out.println("Ім'я: " + getName() + ", Вік: " + getAge() + ", Професія: " + getJob() + ", Стать: "+ Gender);
     }
